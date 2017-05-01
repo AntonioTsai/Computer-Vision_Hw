d0806edd07d00c59ba10660368c04249939a430d
Antonio Tsai <jimmyjack7777@gmail.com>
2017-05-01 10:53:52 +0800

    Init commit

---
+++ Hw6/Hw6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hw6
+//
+//  Created by Antonio Tsai on 01/05/2017.
+//  Copyright © 2017 TTU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
