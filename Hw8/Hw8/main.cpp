bc2b11ff337b028ff412dfc57b2c718476ca2949
Antonio Tsai <jimmyjack7777@gmail.com>
2017-05-15 11:52:11 +0800

    Reverse color and print HuMoments

--- Hw8/Hw8/main.cpp
+++ Hw8/Hw8/main.cpp
@@ -31,6 +31,10 @@ int main( int argc, char** argv )
     
     /// Convert image to gray and blur it
     cvtColor( src, src_gray, CV_BGR2GRAY );
+    
+    // Reverse color
+    bitwise_not ( src_gray, src_gray );
+    
     blur( src_gray, src_gray, Size(3,3) );
     
     /// Create Window
@@ -66,7 +70,21 @@ void thresh_callback(int, void* )
     /// Get the moments
     vector<Moments> mu(contours.size() );
     for( int i = 0; i < contours.size(); i++ )
-    { mu[i] = moments( contours[i], false ); }
+    {
+        mu[i] = moments( contours[i], false );
+    
+        /// Draw HuContours
+//        Moments mom = moments(contours[0]);
+//        double hu[7];
+//        HuMoments(mom, hu);
+        
+        double hu[7];
+        HuMoments(mu[i], hu);
+        for (int j = 0; j < 7; j++) {
+            cout << "HuMoments " << j << " " << hu[j] << endl;
+        }
+        cout << endl;
+    }
     
     ///  Get the mass centers:
     vector<Point2f> mc( contours.size() );
@@ -90,7 +108,7 @@ void thresh_callback(int, void* )
     printf("\t Info: Area and Contour Length \n");
     for( int i = 0; i< contours.size(); i++ )
     {
-        printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
+//        printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
         Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
         drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
         circle( drawing, mc[i], 4, color, -1, 8, 0 );
