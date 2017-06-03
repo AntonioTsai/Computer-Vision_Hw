6e4bf6afc4e326b371c9a8304ce229155f42e665
Antonio Tsai <jimmyjack7777@gmail.com>
2017-06-03 10:37:58 +0800

    Hw10 complete

--- Hw10/Hw10/main.cpp
+++ Hw10/Hw10/main.cpp
@@ -5,11 +5,72 @@
 //  Created by Antonio Tsai on 03/06/2017.
 //  Copyright © 2017 TTU. All rights reserved.
 //
+//  From: http://qtandopencv.blogspot.tw/2013/10/opencv-and-color-quantization-01-kmeans.html
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+int main(int argc, const char * argv[])
+{
+    cv::Mat src = cv::imread("/Users/Antonio/Documents/openCV/perspective05.jpg");
+    if(src.empty()){
+//        std::cerr<"can't read the image"<std::endl;
+        return -1;
+    }
+    
+    //step 1 : map the src to the samples
+    cv::Mat samples(src.total(), 3, CV_32F);
+    auto samples_ptr = samples.ptr<float>(0);
+    for( int row = 0; row != src.rows; ++row){
+        auto src_begin = src.ptr<uchar>(row);
+        auto src_end = src_begin + src.cols * src.channels();
+        //auto samples_ptr = samples.ptr<float>(row * src.cols);
+        while(src_begin != src_end){
+            samples_ptr[0] = src_begin[0];
+            samples_ptr[1] = src_begin[1];
+            samples_ptr[2] = src_begin[2];
+            samples_ptr += 3; src_begin +=3;
+        }
+    }
+    
+    //step 2 : apply kmeans to find labels and centers
+    int clusterCount = 3;
+    cv::Mat labels;
+    int attempts = 5;
+    cv::Mat centers;
+    cv::kmeans(samples, clusterCount, labels,
+               cv::TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS,
+                                10, 0.01),
+               attempts, cv::KMEANS_PP_CENTERS, centers);
+    
+    //step 3 : map the centers to the output
+    cv::Mat new_image(src.size(), src.type());
+    for( int row = 0; row != src.rows; ++row){
+        auto new_image_begin = new_image.ptr<uchar>(row);
+        auto new_image_end = new_image_begin + new_image.cols * 3;
+        auto labels_ptr = labels.ptr<int>(row * src.cols);
+        
+        while(new_image_begin != new_image_end){
+            int const cluster_idx = *labels_ptr;
+            auto centers_ptr = centers.ptr<float>(cluster_idx);
+            new_image_begin[0] = centers_ptr[0];
+            new_image_begin[1] = centers_ptr[1];
+            new_image_begin[2] = centers_ptr[2];
+            new_image_begin += 3; ++labels_ptr;
+        }
+    }
+    
+    cv::Mat binary;
+    cv::Canny(new_image, binary, 30, 90);
+    
+    cv::imshow("original", src);
+    cv::imshow("binary", binary);
+    cv::imshow( "clustered image", new_image );
+    
+    cv::waitKey();
+    
     return 0;
 }
